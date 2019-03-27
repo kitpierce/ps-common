bf998fcd12a9a3dcb33ec3af216d682a7983159b
kitpierce <kitpierce@users.noreply.github.com>
2019-03-27 11:10:15 -0500

    Create Get-Clipboard.ps1

---
+++ shared-tools/Get-Clipboard.ps1
@@ -0,0 +1,32 @@
+function Get-Clipboard {
+    [CmdletBinding()]
+    param (
+        # Use raw text
+        [Parameter(Position=0)]
+        [switch]$Raw,
+
+        # Trim text
+        [Parameter(Position=1)]
+        [switch]$Trim,
+
+        # Uppercase text
+        [Parameter(Position=2)]
+        [switch]$ToUpper
+    )
+    #[void][System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+    Add-Type -AssemblyName System.Windows.Forms
+	$textBox = New-Object System.Windows.Forms.TextBox
+	$textBox.Multiline = $true
+	$textBox.Paste()
+	if ( !$Raw ) {
+	    $arrText = $textBox.Text.Split("`r`n") | Where-Object { $_ }
+	    $result = @()
+	    foreach ( $line in $arrText ) {
+		    if ( $Trim ) { $line = $line.Trim() -Replace "^\W*",'' }
+	        if ( $ToUpper ) { $line = $line.ToUpper() }
+	        $result += $line
+	    }
+        Return $result
+    }
+	Return $textBox.Text
+}
